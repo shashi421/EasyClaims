0c5d0c9f7052d162de675d4088864815724a4159
kalyanshashi <shashikalyan421@gmail.com>
2019-01-10 00:26:18 +0530

    Update urls.py

--- easyclaims/easyclaims/urls.py
+++ easyclaims/easyclaims/urls.py
@@ -14,10 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-import claims
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/', claims.views.hello_world),
+    path('claims/', include('claims.urls')),
 ]
