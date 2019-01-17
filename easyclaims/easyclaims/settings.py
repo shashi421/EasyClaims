bfd4ead7f68005f4c5914436d737c34d6aba39dc
kalyanshashi <shashikalyan421@gmail.com>
2019-01-18 01:45:35 +0530

    Update settings.py

--- easyclaims/easyclaims/settings.py
+++ easyclaims/easyclaims/settings.py
@@ -66,7 +66,7 @@ REST_FRAMEWORK = {
 DJOSER = {
     'PASSWORD_RESET_CONFIRM_URL': '#/password/reset/confirm/{uid}/{token}',
     'ACTIVATION_URL': '#/activate/{uid}/{token}',
-    'SEND_ACTIVATION_EMAIL': True,
+    'SEND_ACTIVATION_EMAIL': False,
     'SERIALIZERS': {},
 }
 
