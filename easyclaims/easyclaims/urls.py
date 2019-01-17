ddba50eedce25ab4f7124be8aaa7e5fdfb85a700
kalyanshashi <shashikalyan421@gmail.com>
2019-01-18 01:39:50 +0530

    added djoser login authenicaton url

--- easyclaims/easyclaims/urls.py
+++ easyclaims/easyclaims/urls.py
@@ -14,10 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.conf.urls import url
+from rest_framework_simplejwt import views as jwt_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('claims/', include('claims.urls')),
     path('dialogflow/', include('dialogflow.urls')),
+    re_path(r'^api/', include('djoser.urls')),
+    re_path(r'^api/', include('djoser.urls.jwt')),
+    path('auth/', include('djoser.urls.authtoken')),
+    #path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    #path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
 ]
