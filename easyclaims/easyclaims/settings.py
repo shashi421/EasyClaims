2a5b047020ddf9ac1145a5ba0e6577c3c9d2ba82
kalyanshashi <shashikalyan421@gmail.com>
2019-01-18 12:54:41 +0530

    Update settings.py

--- easyclaims/easyclaims/settings.py
+++ easyclaims/easyclaims/settings.py
@@ -75,7 +75,7 @@ EMAIL_USE_SSL = True
 DJOSER = {
     'PASSWORD_RESET_CONFIRM_URL': '#/password/reset/confirm/{uid}/{token}',
     'ACTIVATION_URL': '#/activate/{uid}/{token}',
-    'SEND_ACTIVATION_EMAIL': True,
+    'SEND_ACTIVATION_EMAIL': False,
     'SERIALIZERS': {},
 }
 TEMPLATES = [
