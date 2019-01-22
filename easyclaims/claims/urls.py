2b7145ea6cbc51d9a0acd3766390001ab46dfc03
shashi421 <k.s.kalyan24@gmail.com>
2019-01-22 13:37:13 +0000

    Update urls.py

--- easyclaims/claims/urls.py
+++ easyclaims/claims/urls.py
@@ -5,6 +5,7 @@ from .views import ClaimViewList,ClaimViewDetail
 urlpatterns = [
     path('claims/', ClaimViewList.as_view()),
     path('claims/<int:pk>/', ClaimViewDetail.as_view()),
+    path('claims/<int:claimNo>/',ClaimStatusDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
