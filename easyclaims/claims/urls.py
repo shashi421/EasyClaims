776e1664677d6ac06a5f1bfa4e08a8b763d188aa
shashi421 <k.s.kalyan24@gmail.com>
2019-01-23 12:07:24 +0530

    Update urls.py

--- easyclaims/claims/urls.py
+++ easyclaims/claims/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 from claims.views import ClaimViewList,ClaimViewDetail,ClaimStatusDetail
+from . import views
 
 urlpatterns = [
     path('claims/', ClaimViewList.as_view()),
     path('claims/<int:pk>/', ClaimViewDetail.as_view()),
     path('claimstatus/<int:pk>/',ClaimStatusDetail.as_view()),
+    path('claimhelper/', views.dialogFLowClaimHelper)
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
