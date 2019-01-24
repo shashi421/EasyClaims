4b6f40bdee79be3f6dd447c0f4a977b7be5b4513
shashi421 <k.s.kalyan24@gmail.com>
2019-01-24 13:29:34 +0000

    Update models.py

--- easyclaims/claims/models.py
+++ easyclaims/claims/models.py
@@ -14,6 +14,7 @@ class List(models.Model):
       name=models.CharField(max_length=100)
       dob = models.DateField()
       address = models.CharField(max_length=500)
+      location=models.CharField(max_length=500)
       policyNo=models.CharField(max_length=8)
       claimNo =random.choice(range(1,8))
       typeOfClaim=models.CharField(max_length=6, choices=CLAIM_CHOICES, default='')
